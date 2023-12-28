149e3a9e9bba6eeffb8345b3ef5fd7439c6650b6
kang youngmin <kymtion@gmai.com>
2023-12-28 16:09:43 +0900

    Initial Commit

---
+++ dayTodo/dayTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  dayTodoApp.swift
+//  dayTodo
+//
+//  Created by 강영민 on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct dayTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
