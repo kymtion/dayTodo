1d9ebd807014693ad19c704518160d6be247934b
kang youngmin <kymtion@gmai.com>
2024-01-18 18:19:02 +0900

    1.3.1 버전 파이어베이스와 애드몹 연동

--- dayTodo/dayTodoApp.swift
+++ dayTodo/dayTodoApp.swift
@@ -2,9 +2,21 @@
 
 import SwiftUI
 import GoogleMobileAds
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct dayTodoApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     init() {
           // AdMob SDK 초기화
